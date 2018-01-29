3c1c28c3ebb409dacff7530f281ffb42369788bb
Felipe Flores <felipe.flores@rea-group.com>
2018-01-29 16:18:14 +1100

    hspec - level03 solutions.

--- level03/tests/Test.hs
+++ level03/tests/Test.hs
@@ -41,7 +41,31 @@ main = do
       -- Don't worry if you don't get all of these done. :)
 
       -- 1) The '<topic>/add' route will respond with an error when given an empty comment
-      -- 2) The '<topic>/view' route will respond correctly when given a topic
-      -- 3) The '<topic>/view' route will respond with an error when given an empty topic
-      -- 4) A gibberish route will return a 404
+      describe "add topic" $ do
 
+        it "should respond with OK for a valid topic" $
+          post "/add-topic-spec/add" "Very good topic" `shouldRespondWith` 200
+
+        it "should respond with Bad Request for an empty comment" $
+          post "/add-topic-spec/add" "" `shouldRespondWith` 400
+
+      -- 2) The '<topic>/view' route will respond correctly when given a topic
+      describe "view topic" $ do
+
+        it "should respond OK for a view request" $
+          get "/add-topic-spec/view"  `shouldRespondWith` 200
+
+      -- 3) The '<topic>/view' route will respond with an error when given an empty topic
+        xit "should respond BadRequest for missing topics" $
+          get "view"  `shouldRespondWith` 400
+          --pendingWith "we get 404 instead of 400, there's no way we can get that?"
+          -- FIXME: pending or perndingWith cause import problems
+
+      -- 4) A gibberish route will return a 404
+      describe "invalid requests" $ do
+
+        it "should respond with NotFound for a invalid GET requests" $
+          get "/foo/bar" `shouldRespondWith` 404
+
+        it "should respond with NotFound for a invalid POST requests" $
+          post "/foo/bar" "Very good topic" `shouldRespondWith` 404
